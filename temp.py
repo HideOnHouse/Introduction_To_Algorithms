3a0255c174e5bd279609753eb66904e22b3178f1
HideOnHouse <fffuro@kookmin.ac.kr>
2021-02-22 18:16:27 +0900

    Add programmers

--- temp.py
+++
@@ -1,7 +0,0 @@
-from itertools import permutations
-
-if __name__ == '__main__':
-    for i in permutations("고노무현대통령", 7):
-        temp = "".join(i)
-        if "고무통" in temp:
-            print(temp)
